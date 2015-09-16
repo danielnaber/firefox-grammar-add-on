37fa50a0c99cfa996ee881fb23d601275232ee37
Daniel Naber <naber@danielnaber.de>
2015-09-16 10:45:34 +0200

    make first version work

--- index.js
+++ index.js
@@ -1,50 +1,38 @@
 var {Cc, Ci} = require("chrome");
 
-var gcSvc = Cc["@mozilla.org/grammarcheck;1"].
-				getService(Ci.nsIEditorGrammarCheck);
-
+var gcSvc = Cc["@mozilla.org/grammarcheck;1"].getService(Ci.nsIEditorGrammarCheck);
 var xmlhttp = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"].createInstance(Ci.nsIXMLHttpRequest);
 
-function handleWord(word) 
-{
+let checkerUrl = "https://languagetool.org:8081/";
+
+function handleText(text) {
 	var oReq = xmlhttp;
 	oReq.addEventListener("load", function(result) {
-		console.log("LOAD!!");
-		console.log(result);
-		console.log(oReq.responseXML);
 		var xml = oReq.responseXML;
 		var errors = xml.getElementsByTagName("error");
 		var startPositions = [];
 		var endPositions = [];
+		var replacements = [];
 		for (var i = 0; i < errors.length; i++) {
-			console.log("ERROR: " + errors[i].getAttribute("offset") + ", len: " + errors[i].getAttribute("errorlength"));
+			//console.error("Got match: " + errors[i].getAttribute("offset") + ", len: " + errors[i].getAttribute("errorlength"));
 			var startPos = parseInt(errors[i].getAttribute("offset"));
 			var endPos = startPos + parseInt(errors[i].getAttribute("errorlength"));
 			startPositions.push(startPos);
 			endPositions.push(endPos);
-			// msg, replacements (#)
+			replacements.push(errors[i].getAttribute("replacements"));
+			// TODO: use 'msg' attribute
 		}
-		//gcSvc.errorsFound(startPositions, endPositions, startPositions.length);
+		gcSvc.errorsFound(startPositions, endPositions, startPositions.length);
 		for (var j = 0; j < errors.length; j++) {
-			//gcSvc.addSuggestionForError(0, "[this will be replaced]");
-
+			// TODO: split at '#'
+			gcSvc.addSuggestionForError(j, replacements[j]);
 		}
-		console.log("START: " + startPositions);
-		console.log("END: " + endPositions);
 	});
-	oReq.open("POST", "https://languagetool.org:8081/", true);
+	oReq.open("POST", checkerUrl, true);
 	oReq.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	var data = "language=en&text=this is an test";
+	//var data = "language=en&text=" + text;
+	var data = "autodetect=1&text=" + text;
 	oReq.send(data);
-	console.log("POSTED");
-
-	var startPos = [0,4];
-	var endPos = [2,9];
-	
-	gcSvc.errorsFound(startPos, endPos, startPos.length);
-	gcSvc.addSuggestionForError(0, "[this will be replaced]");
-	gcSvc.addSuggestionForError(1, "[another suggestion]");
 }
-
 				
-gcSvc.registerAddon(handleWord);
+gcSvc.registerAddon(handleText);
