c9fbfe6ffafa239b6244c1136ff17cb19475fb19
Daniel Naber <naber@danielnaber.de>
2015-09-20 16:44:17 +0200

    show bug still exists when using one way to call setTimeout

--- index-show-replace-bug.js
+++ index-show-replace-bug.js
@@ -4,21 +4,27 @@ var gcSvc = Cc["@mozilla.org/grammarcheck;1"].
 				getService(Ci.nsIEditorGrammarCheck);
 var { setTimeout, clearTimeout } = require("sdk/timers");	
 
-function handleWord(word) 
-{
+var currentText;
 
-	console.error("Handle Word");
+function dispatchErrors(text)
+{
+	if(currentText != text)
+		return;
 	
 	var startPos = [1,4];
 	var endPos = [2,9];
-
-	setTimeout(function() {
-		gcSvc.errorsFound(startPos, endPos, startPos.length);
-		gcSvc.addSuggestionForError(0, "replaced", "msg1");
-		gcSvc.addSuggestionForError(1, "suggestion1", "msg2");
-		gcSvc.addSuggestionForError(1, "suggestion2", "msg2");
-        }, 500)
+	gcSvc.errorsFound(startPos, endPos, startPos.length);
+	gcSvc.addSuggestionForError(0, "replaced", "msg1");
+	gcSvc.addSuggestionForError(1, "suggestion1", "msg2");
+	gcSvc.addSuggestionForError(1, "suggestion2", "msg2");
 }
 
+function handleText(text) 
+{
+	currentText = text;
+	console.error("Handle Word");
+	//setTimeout(dispatchErrors(text), 500)  // this works
+	setTimeout(function() { dispatchErrors(text) }, 500)  // this doesn't work on replace
+}
 				
-gcSvc.registerAddon(handleWord);
+gcSvc.registerAddon(handleText);
